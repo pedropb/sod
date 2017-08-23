716ca4893ccf700cbd0a361c62b5f8e46c19df99
Pedro Baracho <pedropbaracho@gmail.com>
2017-08-23 14:47:32 -0300

    Adding schemaspy

---
+++ schemaspy/sod-schemaspy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DB=sod
+SCHEMA=sod
+HOST=db:5432
+USER=sod
+PASSWORD=1234
+docker run --rm --network docker_sod_default -v="$PWD/output":/output matthewdodds/docker-schemaspy-postgres bash -c "java -jar schemaSpy.jar -t pgsql -db $DB -host $HOST -dp postgresql-jdbc4.jar -u $USER -p $PASSWORD -s public -o /output"
