89011abd371ba5b4c82dc6c60d48f2bd63bd702b
Pedro Baracho <pedropbaracho@gmail.com>
2017-10-20 21:58:18 -0200

    Including file attribute and improving comments

--- src/main/java/geotech/sod/DataImporterConfig.java
+++ src/main/java/geotech/sod/DataImporterConfig.java
@@ -9,25 +9,36 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * This class holds the configuration for an Import Operation.
+ * 
  * See also: 
  *  - src/main/webapp/js/definitions/importDataPanel.js 
  *  - src/main/java/DataImporter.java
  */
 public class DataImporterConfig {
+
+    // true to allow deleting data, false to block it
     public final boolean cleanDb;
+
+    // true to delete each definition, false to not touch them
     public final boolean delUsers;
     public final boolean delGroups;
     public final boolean delModules;
     public final boolean delTransactions;
     public final boolean delActivities;
     public final boolean delConflicts;
+
+    // true to delete all solutions, false will keep standard behavior
+    // standard behavior is to delete solutions for users and groups
+    // that had their accesses changed
     public final boolean delUsersSolutions;
     public final boolean delGroupsSolutions;
 
-    public DataImporterConfig(boolean cleanDb, boolean delUsers, boolean delGroups, boolean delModules, 
-        boolean delTransactions, boolean delActivities, boolean delConflicts, boolean delUsersSolutions, 
-        boolean delGroupsSolutions  ) {
+    // the path for the XLS(X) file with the import data
+    public final String importDataFilePath;
+
+    public DataImporterConfig(String importDataFilePath, boolean cleanDb, boolean delUsers, boolean delGroups, boolean delModules, boolean delTransactions, boolean delActivities, boolean delConflicts, boolean delUsersSolutions, boolean delGroupsSolutions  ) {
         super();
+        this.importDataFilePath = importDataFilePath;
         this.cleanDb = cleanDb;
         this.delUsers = delUsers;
         this.delGroups = delGroups;
@@ -58,6 +69,6 @@ public class DataImporterConfig {
         boolean delUsersSolutions = uploadData.get("users_solutions") != null ? uploadData.get("users_solutions").getValue().equals("on") : false;
         boolean delGroupsSolutions = uploadData.get("groups_solutions") != null ? uploadData.get("groups_solutions").getValue().equals("on") : false;
 
-        return new DataImporterConfig(clean_db, delUsers, delGroups, delModules, delTransactions, delActivities, delConflicts, delUsersSolutions, delGroupsSolutions);
+        return new DataImporterConfig(sFile, clean_db, delUsers, delGroups, delModules, delTransactions, delActivities, delConflicts, delUsersSolutions, delGroupsSolutions);
     }
 }
